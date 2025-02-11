b611baabb664669f8e73c2ceec1ce80be38109d3
O.J. Paul <jpaul9327@gmail.com>
2025-02-11 20:59:19 +0300

    Add custom passwordbox that supports binding

---
+++ Chente.Desktop/Controls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chente.Desktop.Controls;
+/// <summary>
+/// Interaction logic for BindablePasswordBox.xaml
+/// </summary>
+public partial class BindablePasswordBox : UserControl
+{
+    public BindablePasswordBox()
+    {
+        InitializeComponent();
+    }
+
+    private static bool isUserTyping;
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(BindablePasswordBox), new PropertyMetadata(string.Empty, HandlePasswordChanged));
+
+    private static void HandlePasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is BindablePasswordBox bindablePasswordBox) bindablePasswordBox.UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (!isUserTyping) DefaultPasswordBox.Password = Text;
+    }
+
+    private void DefaultPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+    {
+        isUserTyping = true;
+        Text = DefaultPasswordBox.Password;
+        isUserTyping = false;
+    }
+}
